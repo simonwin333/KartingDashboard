79d021e322f1c1c7aec51edacc14cebf17163ffa
simonwin333 <simonwincq@gmail.com>
2026-03-11 19:36:27 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker - Karting Dashboard v4.0
-const CACHE_NAME = 'karting-v4.35';
+const CACHE_NAME = 'karting-v4.36';
 
 // Ne pas mettre de chemins absolus - utiliser des chemins relatifs
 const ASSETS = [
